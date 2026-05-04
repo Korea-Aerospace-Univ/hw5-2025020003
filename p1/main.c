de3718c394ba48ecd1a8b94fab70a866fb9780b7
2025020003 <woongsman12@gmail.com>
2026-05-04 16:00:05 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -4,11 +4,10 @@ int main(void){
     int money, a=900,b=750,c=200;
     int i,j,k;
     bool is_correct=0;
-    printf("금액 입력:");
     scanf("%d",&money);
-    for(i=1;i<=11;i++){
-        for(j=2;j<=12;j+=2){
-            for(k=1;k<=50;k++){
+    for(i=1;i<=(money/a);i++){
+        for(j=2;j<=(money/b);j+=2){
+            for(k=1;k<=(money/c);k++){
                 if(k<i||k<j){
                     if((a*i)+(b*j)+(c*k)==money){
                         printf("%d %d %d\n",i,j,k);
